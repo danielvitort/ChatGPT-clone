8c94c4df4fbe9f9c3be2907a607111eac3a8c382
danielvitort <danielvitorzero@gmail.com>
2024-01-31 23:18:06 -0300

    first

---
+++ README.md
@@ -0,0 +1,9 @@
+# ChatGPT
+
+Projeto em React + Typescript + TailwindCSS utilizando Api do ChatGPT.
+
+## Instalação
+- `npm install`
+
+## Para rodar
+- `npm run dev`
---
+++ src/app/components/ChatArea.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useRef } from "react";
+import { Chat } from "../types/Chat"
+import { ChatMessageItem } from "./ChatMessageItem";
+import { ChatMessageLoading } from "./ChatMessageLoading";
+import { ChatPlaceholder } from "./ChatPlaceholder";
+
+type Props = {
+    chat: Chat | undefined;
+    loading: boolean;
+}
+
+export const ChatArea = ({ chat, loading }: Props) => {
+    const scrollArea = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        scrollArea.current?.scrollTo(0, scrollArea.current?.scrollHeight);
+    }, [loading, chat?.messages.length])
+
+    return (
+        <section className="flex-auto h-0 overflow-y-scroll">
+            {!chat && <ChatPlaceholder />}
+            {chat && chat.messages.map(item => (
+                <ChatMessageItem 
+                    key={item.id}
+                    item={item}
+                />
+            ))}
+            {loading && <ChatMessageLoading/>}
+        </section>
+    )
+}
